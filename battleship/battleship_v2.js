ac26c8ec5188a8eaad97f003077821d00bfd8753
lorenz1221 <gamerbeast898@gmail.com>
2025-04-02 20:30:59 +0800

    battleshipv2

--- battleship/battleship_v2.js
+++ battleship/battleship_v2.js
@@ -1,10 +1,11 @@
-//declaring variable
+// declaring variable
 
-var randomLoc = Math.floor(Math.random() * 5);
+
+var randomLoc = Math.floor(Math.random() *5);
 
 var location1 = randomLoc;
-var location2 = location + 1;
-var location3 = location + 1; 
+var location2 = location1 +1;
+var location3 = location2 +1;
 
 var guess
 var hits = 0;
@@ -15,7 +16,7 @@ var isSunk = false;
 // Game logic
 
 while (isSunk == false){
-    guess = prompt("Ready!. Aim. Fire! (Enter No.6 from 0-6");
+    guess = prompt("Ready!. Aim!. Fire! (enter number 0-6)");
 
     if (guess == null) {
         alert("Thank you for playing!");
@@ -23,26 +24,27 @@ while (isSunk == false){
     }
 
     if (guess < 0 || guess > 6){
-        alert("Please enter a valid cell number");
+        alert("Please enter a valid cell number!");
     }else {
         guesses = guesses + 1;
-        alert("hits")
-        if(guess == location1 || guess == location2 || guess == location3) {
+        if(guess == location1 || guess == location2 || guess == location3){
             hits = hits + 1;
-            alert("Hit!");
-            if(hits == 3){
+            alert ("hits")
+            if (hits == 3) {
                 inSunk = true;
-                alert ("You sank my battleship!")
-                
+                alert("You sank my battleship!")
             }
         }
-        else alert("Miss!");
+        else{
+            alert("Miss!")
+        }
     }
 }
-var accuracy = (guesses > 0 ? (hits / guesses) * 100 : 0).toFixed(2);
 
-var stats = "You took " + guesses + " guesses to sink the battleship! " + "\n" + "Accuracy: " + accuracy + "%";
+var accuracy = (guess > 0 ? (hits / guess) * 100 : 0). toFixed(3);
+
+var stats = "You took " +guesses + " guesses to sink the battleship! " + "\n" + "Accuracy: " + accuracy + "%";
 
 var stats = "You sank my battleship at: " + guesses + "guesses" + "and your accuracy is: " + (3/guesses);
 
-alert(stats); 
+alert(stats);
