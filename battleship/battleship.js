04a6eba978fc2f80e7fafe36cfabeb992b59fad0
lorenz1221 <gamerbeast898@gmail.com>
2025-04-21 20:05:22 +0800

    battleship.js

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -12,24 +12,25 @@ var isSunk = false;
 // Game logic
 
 while (isSunk == false){
-    guess = prompt("Ready!. Aimi. Fire! (enter numbe)");
+    guess = prompt("Ready!. Aim!. Fire! (enter number 0-6)");
     if (guess < 0 || guess > 6){
-        alert("lease enter a valid cell number");
+        alert("Please enter a valid cell number!");
     }else {
         guesses = guesses + 1;
-        alert("hits")
-        if(guess == location1 || guess == location2 || guess == location3) {
+        if(guess == location1 || guess == location2 || guess == location3){
             hits = hits + 1;
-            if(hits == 3){
+            alert ("hits")
+            if (hits == 3) {
                 inSunk = true;
-                alert ("You sank my battleship!")
-                
+                alert("You sank my battleship!")
             }
         }
-        else alert("Miss!");
+        else{
+            alert("Miss!")
+        }
     }
 }
 
-var stats = "You sank my battleship at: " + guesses + "guesses" + "and your accuracy is:" + (3/guesses);
+var stats = "You sank my battleship at: " + guesses + "guesses" + "and your accuracy is: " + (3/guesses);
 
 alert(stats);
